0c91ad7c763138577acaaf02000b9f319e96d403
Ansh Dassani <anshdassani4@gmail.com>
2020-06-06 21:51:38 +0530

    Nthing

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -30,7 +30,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Color(0xff1F1F1F),
